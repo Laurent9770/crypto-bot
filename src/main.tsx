36d3ba443658a7febc48ff1651a5147745925bcf
Your Name <your.email@example.com>
2025-07-21 16:42:37 +0700

    fix: resolve Buffer polyfill issue for Vite

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
-import { Buffer } from 'buffer';
-window.Buffer = Buffer;
-import process from 'process';
-window.process = process;
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
